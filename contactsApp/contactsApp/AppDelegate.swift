687b77070984a2fdb415ba46a048062ae099b7ad
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-06-15 22:48:00 +0200

    FireBase installation

--- contactsApp/contactsApp/AppDelegate.swift
+++ contactsApp/contactsApp/AppDelegate.swift
@@ -1,6 +1,7 @@
 
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -9,6 +10,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
