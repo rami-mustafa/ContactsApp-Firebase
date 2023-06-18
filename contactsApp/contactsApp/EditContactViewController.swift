121a804d1274291eb423fe749b4eb19f96896920
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-06-18 20:03:27 +0200

    Edit Contact ViewController

--- contactsApp/contactsApp/EditContactViewController.swift
+++ contactsApp/contactsApp/EditContactViewController.swift
@@ -1,6 +1,7 @@
  
 
 import UIKit
+import Firebase
 
 class EditContactViewController: UIViewController {
 
@@ -8,14 +9,40 @@ class EditContactViewController: UIViewController {
    
     @IBOutlet weak var personPhoneTextField: UITextField!
     
+    var person:Contacts?
+    var ref:DatabaseReference!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        ref = Database.database().reference()
+
+        
+        if let p = person {
+            
+            personNameTextField.text = p.kisi_ad
+            personPhoneTextField.text = p.kisi_tel
+        }
 
      }
     
 
     @IBAction func save(_ sender: Any) {
+        
+        if let p = person  , let name = personNameTextField.text , let phone = personPhoneTextField.text {
+          
+            personUpdate(kisi_id:p.kisi_id! ,kisi_ad:name , kisi_tel:phone)
+         }
+        
+    }
+    func personUpdate(kisi_id:String ,kisi_ad:String , kisi_tel:String ) {
+        
+ 
+        ref.child("kisiler").child(kisi_id).updateChildValues(["kisi_id":"","kisi_ad":kisi_ad,"kisi_tel":kisi_tel])
+        
+     
+        
     }
     
 
