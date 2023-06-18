9221a358f8327154401ca42f4f2feb11296a984c
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-06-18 20:02:53 +0200

    Person Detail View

--- contactsApp/contactsApp/PersonDetailViewController.swift
+++ contactsApp/contactsApp/PersonDetailViewController.swift
@@ -7,8 +7,18 @@ class PersonDetailViewController: UIViewController {
     @IBOutlet weak var personNameLabel: UILabel!
     @IBOutlet weak var personPhoneLabel: UILabel!
 
+    var person:Contacts?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        if let p = person {
+            
+            personNameLabel.text = p.kisi_ad
+            personPhoneLabel.text = p.kisi_tel
+        }
 
      }
     
