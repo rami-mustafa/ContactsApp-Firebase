db090770cb8a389475f8c2953044b69dd6686626
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-06-18 19:26:26 +0200

    create new contact

--- contactsApp/contactsApp/CreateNewPersonViewController.swift
+++ contactsApp/contactsApp/CreateNewPersonViewController.swift
@@ -1,6 +1,7 @@
  
 
 import UIKit
+import Firebase
 
 class CreateNewPersonViewController: UIViewController {
 
@@ -10,14 +11,38 @@ class CreateNewPersonViewController: UIViewController {
     
     @IBOutlet weak var personPhoneTextField: UITextField!
     
+    var ref:DatabaseReference!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        ref = Database.database().reference()
+
+
      }
+    
+    
+    
     
  
     @IBAction func add(_ sender: Any) {
+        
+        if let name = personNameTextField.text , let phone = personPhoneTextField.text {
+            
+            addContact(kisi_ad: name, kisi_tel: phone)
+        }
+        
+    }
+    
+    func addContact(kisi_ad:String , kisi_tel:String ) {
+        
+        let dict:[String:Any] = ["kisi_id":"","kisi_ad":kisi_ad,"kisi_tel":kisi_tel]
+        
+        let newRef = ref?.child("kisiler").childByAutoId()
+        
+        newRef?.setValue(dict)
+        
     }
     
 }
